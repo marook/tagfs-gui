d89ab4900012da4dbf176254aa2c37dbdfe48c56
Markus Pielmeier <markus.pielmeier@gmx.de>
2011-01-10 16:58:31 +0100

    implemented job runner

--- src/modules/tagfs_gui/edit.py
+++ src/modules/tagfs_gui/edit.py
@@ -5,6 +5,8 @@ import gtk.glade
 
 from tag_utils import tag_io
 
+from tagfs_gui import job
+
 class TaggingsListStore(gtk.ListStore):
 
     def __init__(self, taggedDir):
@@ -37,20 +39,34 @@ class TaggingsListStore(gtk.ListStore):
         self.item = tag_io.parseDirectory(self.taggedDir)
 
         self._updateModelFromItem()
-    
 
     def saveTaggings(self):
         tagFileName = os.path.join(self.taggedDir, tag_io.DEFAULT_TAG_FILE_NAME)
 
         tag_io.writeFile(self.item, tagFileName)
 
+class LoadTaggingsJob(object):
+
+    def __init__(self, saveAction, taggingsTreeView, taggingsListStore):
+        self.description = 'Loading taggings...'
+        self.saveAction = saveAction
+        self.taggingsTreeView = taggingsTreeView
+        self.taggingsListStore = taggingsListStore
+
+    def run(self):
+        # TODO do we have to care about synchronization?
+        self.taggingsListStore.loadTaggings()
+
+        self.taggingsTreeView.set_sensitive(True)
+        self.saveAction.set_sensitive(True)
+
 class EditApp(object):
 
     def initTaggingsTreeView(self, taggedPath):
-        v = self.gui.get_object('taggingsTreeView')
+        self.taggingsTreeView = self.gui.get_object('taggingsTreeView')
 
         self.taggingsListStore = TaggingsListStore(taggedPath)
-        v.set_model(self.taggingsListStore)
+        self.taggingsTreeView.set_model(self.taggingsListStore)
 
         def editedContext(cell, path, newText):
             self.taggingsListStore.setContext(path, newText)
@@ -77,7 +93,20 @@ class EditApp(object):
             c.set_resizable(True)
             c.set_sort_column_id(i)
 
-            v.append_column(c)
+            self.taggingsTreeView.append_column(c)
+
+    def initJobRunner(self):
+        self.statusBar = self.gui.get_object('statusbar')
+
+        jobRunnerContextId = self.statusBar.get_context_id('JobRunnerDescriptions')
+        def setJobDescription(desc):
+            if desc is None:
+                self.statusBar.pop(jobRunnerContextId)
+            else:
+                self.statusBar.push(jobRunnerContextId, desc)
+
+        self.jobRunner = job.JobRunner([LoadTaggingsJob(self.saveAction, self.taggingsTreeView, self.taggingsListStore)], setJobDescription)
+        self.jobRunner.start()
 
     def __init__(self, taggedPath):
         self.gui = gtk.Builder()
@@ -86,6 +115,8 @@ class EditApp(object):
         self.gui.add_from_file('src/glade/tagEditDialog.glade')
         self.gui.connect_signals(self)
 
+        self.saveAction = self.gui.get_object('saveAction')
+
         # TODO implement tag file support
         self.initTaggingsTreeView(taggedPath)
 
@@ -93,7 +124,15 @@ class EditApp(object):
         self.editWindow.set_title('tagging ' + taggedPath)
         self.editWindow.show()
 
+        self.initJobRunner()
+
     def quit(self):
+        with self.jobRunner.jobsLock:
+            while len(self.jobRunner.jobs) > 0:
+                self.jobRunner.jobs.pop()
+
+        self.editWindow.set_visible(False)
+
         gtk.main_quit()
 
     def on_saveAction_activate(self, w):
@@ -106,6 +145,8 @@ class EditApp(object):
         self.quit()
 
 def main(args):
+    gtk.gdk.threads_init()
+
     # TODO show popup when no args are passed
     app = EditApp(args[1])
 
---
+++ src/modules/tagfs_gui/job.py
@@ -0,0 +1,48 @@
+import threading
+
+class JobRunner(threading.Thread):
+    
+    def __init__(self, jobs, jobDescCallback):
+        threading.Thread.__init__(self)
+
+        self.jobDescCallback = jobDescCallback
+
+        self.jobsLock = threading.Lock()
+        self.jobs = jobs
+        self.executedJob = None
+
+    def run(self):
+        while True:
+            with self.jobsLock:
+                self.executedJob = None
+
+                if len(self.jobs) == 0:
+                    break
+
+                job = self.jobs.pop(0)
+
+                self.executedJob = job
+
+            try:
+                self.jobDescCallback(job.description)
+
+                job.run()
+            except:
+                # TODO show error message to user and continue
+                pass
+            finally:
+                with self.jobsLock:
+                    self.executedJob = None
+
+                self.jobDescCallback(None)
+
+        # TODO use logger insteaf of print
+        print 'JobRunner is done'
+
+    def stop(self):
+        with self.jobsLock:
+            if not self.executedJob is None:
+                self.executedJob.stop = True
+
+            while len(self.jobs) > 0:
+                self.jobs.pop()
